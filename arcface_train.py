acd347176a771b7b5bdf062897f4374d22f8a452
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-03-30 18:48:58 +0800

    Update arcface_train.py

--- arcface_train.py
+++ arcface_train.py
@@ -21,6 +21,11 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 
+sys.path.append('insightface/recognition/arcface_torch')
+sys.path.append('arcface-pytorch/models')
+
+from backbones import get_model
+from metrics import ArcMarginProduct
 
 import argparse
 
@@ -34,48 +39,6 @@ class FeatureExtractor(nn.Module):
 	def forward(self, x):
 		feats = self.truncated_resnet(x)
 		return feats.view(feats.size(0), -1)
-
-class ArcMarginProduct(nn.Module):
-	r"""Implement of large margin arc distance: :
-		Args:
-			in_features: size of each input sample
-			out_features: size of each output sample
-			s: norm of input feature
-			m: margin
-			cos(theta + m)
-		"""
-	def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False):
-		super(ArcMarginProduct, self).__init__()
-		self.in_features = in_features
-		self.out_features = out_features
-		self.s = s
-		self.m = m
-		self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
-		nn.init.xavier_uniform_(self.weight)
-
-		self.easy_margin = easy_margin
-		self.cos_m = math.cos(m)
-		self.sin_m = math.sin(m)
-		self.th = math.cos(math.pi - m)
-		self.mm = math.sin(math.pi - m) * m
-
-	def forward(self, input, label):
-		# --------------------------- cos(theta) & phi(theta) ---------------------------
-		cosine = F.linear(F.normalize(input), F.normalize(self.weight))
-		sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
-		phi = cosine * self.cos_m - sine * self.sin_m
-		if self.easy_margin:
-			phi = torch.where(cosine > 0, phi, cosine)
-		else:
-			phi = torch.where(cosine > self.th, phi, cosine - self.mm)
-		# --------------------------- convert label to one-hot ---------------------------
-		one_hot = torch.zeros(cosine.size()).type_as(label)
-		one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-		# -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
-		output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
-		output *= self.s
-
-		return output
 
 
 class ArcFace(LightningModule):
@@ -131,10 +94,6 @@ class ArcFace(LightningModule):
 		# return torch.optim.Adam(self.parameters(), lr=1e-4)
 
 def main():
-	sys.path.append('insightface/recognition/arcface_torch')
-	
-	if not opt.structure.startswith('resnet'):
-		from backbones import get_model
 		
 	from facedataset import MXFaceDatasetConventional, MXFaceDatasetBalancedIntraInterClusters, collate_paired_data, MXFaceDatasetFromBin
 
